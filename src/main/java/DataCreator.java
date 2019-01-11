a418c1f214c042cb43be68c19b5677d0e5f9284d
n41r0j <jorian@outlook.com>
2019-01-11 17:23:26 +0100

    updated datacreator

--- src/main/java/DataCreator.java
+++ src/main/java/DataCreator.java
@@ -15,18 +15,26 @@ public class DataCreator {
         JsonArray jsonArray = new Gson().fromJson(new JsonReader(new FileReader(contenders)), JsonArray.class);
 
         Random rand = new Random();
+
+        FileWriter fw = new FileWriter("src/events.json", true);
+        BufferedWriter bfw = new BufferedWriter(fw);
+        PrintWriter out = new PrintWriter(bfw);
+
         for (int i = 0; i < 25; i++) {
             int selection1 = rand.nextInt(28);
             int selection2;
 
+            // 2nd contender cannot be the same as first contender:
             do {
                 selection2 = rand.nextInt(28);
 
             } while (selection2 == selection1);
 
+            // get them as JSON objects
             JsonObject contender1 = (JsonObject) jsonArray.get(selection1);
             JsonObject contender2 = (JsonObject) jsonArray.get(selection2);
 
+            // now create an event in JSON
             JsonObject event = new JsonObject();
             event.addProperty("event_id", "ev00" + i);
             event.addProperty("contender_1_id", contender1.get("contender_id").getAsString());
@@ -35,9 +43,7 @@ public class DataCreator {
             event.addProperty("current_odds_contender_2", rand.nextInt(10) + 2);
             event.addProperty("outcome", rand.nextInt(1) + 1);
 
-            System.out.println(event);
-            FileOutputStream fos = new FileOutputStream(events);
-            fos.write(event.toString().getBytes());
+            out.println(event.toString());
         }
     }
 }
