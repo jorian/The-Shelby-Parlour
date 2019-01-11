ba1344dcb639f546b9bb5ad42ebd4dccd7f2689b
Mehdi Hmidi <mh223vk@student.lnu.se>
2019-01-11 15:00:21 +0100

    WIP: primary refactor

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,11 +2,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
-
 import org.json.JSONObject;
-
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.sql.*;
@@ -76,92 +73,71 @@ public class Program {
     }
 
     /*
-    This method contains 4 while loops to import data from 4 json files: contenders, events, gamblers and wagers.
-    For every json, a new inputstream needs to be created. Then, based on that inputstream, a BufferedReader is created, to
+    For every json, a new inputStream needs to be created. Then, based on that inputstream, a BufferedReader is created, to
     be able to read the file line by line.
-    In the while loop, all data from a json file gets converted to a PreparedStatement, which gets executed such that
-    the data is imported in the right table.
-
+    In the while loop, all data from a json file gets converted to a PreparedStatement, which is then batched en masse into the specified tables.
     Since the schema is different for each table, we need 4 different while loops.
      */
     private static void parseJsonToDB(String dbLocation) throws SQLException, IOException {
         Connection conn = DriverManager.getConnection(dbLocation);
-
-        InputStream inputStream = new FileInputStream(new File("src/contenders.json"));
-        Objects.requireNonNull(inputStream, "InputStream cannot be null");
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-
         conn.setAutoCommit(false); // disables the automatic updating and commits to the database so we can batch statements and significantly improve speed
-        String line;
-        JSONObject jsonObject;
 
         String sqlContender = "INSERT OR IGNORE INTO contenders(contender_id, name, confidence) VALUES(?,?,?)";
         PreparedStatement ppstmtContender = conn.prepareStatement(sqlContender);
-
         File contenders = new File("src/contenders.json");
-
         JsonArray jsonArray = new Gson().fromJson(new JsonReader(new FileReader(contenders)), JsonArray.class);
-
         for (JsonElement jsonElement : jsonArray) {
             JsonObject contender = (JsonObject) jsonElement;
-
             ppstmtContender.setString(1, contender.get("contender_id").getAsString());
             ppstmtContender.setString(2, contender.get("name").getAsString());
             ppstmtContender.setInt(3, contender.get("confidence").getAsInt());
-
             ppstmtContender.addBatch();
         }
         ppstmtContender.executeBatch();
         conn.commit();
 
-        inputStream = new FileInputStream(new File("src/events.json"));
+        String line;
+        JSONObject jsonObject;
+        /*Insert into the events Table*/
+        InputStream  inputStream = new FileInputStream(new File("src/events.json"));
         Objects.requireNonNull(inputStream, "InputStream cannot be null");
-        bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         String sqlEvent = "INSERT OR IGNORE INTO events(event_id, outcome, current_odds_contender_1, current_odds_contender_2, contender_1_id, contender_2_id)VALUES(?,?,?,?,?,?)";
         PreparedStatement ppstmtEvent = conn.prepareStatement(sqlEvent);
-
         while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
             jsonObject = new JSONObject(line);
-
             ppstmtEvent.setString(1, jsonObject.getString("event_id"));
             ppstmtEvent.setInt(2, jsonObject.getInt("outcome"));
             ppstmtEvent.setString(3, jsonObject.getString("contender_1_id"));
             ppstmtEvent.setInt(4, jsonObject.getInt("current_odds_contender_1"));
             ppstmtEvent.setString(5, jsonObject.getString("contender_2_id"));
             ppstmtEvent.setInt(6, jsonObject.getInt("current_odds_contender_2"));
-
             ppstmtEvent.addBatch();
         }
         ppstmtEvent.executeBatch();
         conn.commit();
-
+        /*Insert into the gamblers Table*/
         inputStream = new FileInputStream(new File("src/gamblers.json"));
         Objects.requireNonNull(inputStream, "InputStream cannot be null");
         bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-
         String sqlGambler = "INSERT OR IGNORE INTO gamblers(gambler_id, name, address) VALUES(?,?,?)";
         PreparedStatement ppstmtGambler = conn.prepareStatement(sqlGambler);
-
         while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
             jsonObject = new JSONObject(line);
             ppstmtGambler.setString(1, jsonObject.getString("gambler_id"));
             ppstmtGambler.setString(2, jsonObject.getString("name"));
             ppstmtGambler.setString(3, jsonObject.getString("address"));
-
             ppstmtGambler.addBatch();
         }
         ppstmtGambler.executeBatch();
         conn.commit();
-
+/*Insert into the wagers Table*/
         inputStream = new FileInputStream(new File("src/wagers.json"));
         Objects.requireNonNull(inputStream, "InputStream cannot be null");
         bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-
         String sqlWager = "INSERT OR IGNORE INTO wagers(wager_id, event_id,gambler_id,odds,selection,stake,date_of_wager)VALUES(?,?,?,?,?,?,?)";
-
         PreparedStatement ppstmtWager = conn.prepareStatement(sqlWager);
         while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
@@ -173,7 +149,6 @@ public class Program {
             ppstmtWager.setInt(5, jsonObject.getInt("selection"));
             ppstmtWager.setInt(6, jsonObject.getInt("stake"));
             ppstmtWager.setString(7, jsonObject.getString("date_of_wager"));
-
             ppstmtWager.addBatch();
         }
         ppstmtWager.executeBatch();
