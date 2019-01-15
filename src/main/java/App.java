74ea334c5fd95302b59eee8a98327ba245cc8c94
Mehdi Hmidi <mh223vk@student.lnu.se>
2019-01-15 02:56:26 +0100

    WIP refactor. Next to do is implementing the 3rd feature

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,101 +7,81 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class App {
-    @SuppressWarnings("Duplicates")
     public static void main(String[] args) {
         System.out.println("PEAKY BLINDERS Business Improver");
         System.out.println("What you want, eh?\n");
 
         int choice = getMenuChoice(
                 "\t1. Show average winnings\n" +
-                "\t2. Show possible cheaters\n" +
-                "\t3. View popular boxing matches\n");
-
+                        "\t2. Show possible cheaters\n" +
+                        "\t3. View popular boxing matches\n");
         switch (choice) {
-
-            // Average winnings
-            // for an Event, Day or just overall average.
             case 1:
-                System.out.println("Choice is 1");
-
+                System.out.println("Choice selected: 1");
                 int secondChoice = getMenuChoice(
                         "Show average winnings for:\n" +
                                 "\t1. Event\n" +
                                 "\t2. Day\n" +
                                 "\t3. Show overall average\n"
                 );
-
                 switch (secondChoice) {
                     case 1: {
                         String statement = "SELECT * FROM events;";
                         ArrayList results = doSQLStatement(statement);
                         String eventID = getEventIDInput("Enter an event ID: ", results);
-
                         statement = String.format(
                                 "SELECT wager_id, odds, gambler_id, stake " +
-                                "FROM wagers " +
-                                "WHERE event_id = '%s' AND selection = (  " +
-                                    "SELECT selection " +
-                                    "FROM wagers AS w1, events " +
-                                    "WHERE events.event_id = '%s' AND w1.selection <> events.outcome )" +
-                                "ORDER BY stake; ", eventID, eventID);
-
+                                        "FROM wagers " +
+                                        "WHERE event_id = '%s' AND selection = (  " +
+                                        "SELECT selection " +
+                                        "FROM wagers AS w1, events " +
+                                        "WHERE events.event_id = '%s' AND w1.selection <> events.outcome )" +
+                                        "ORDER BY stake; ", eventID, eventID);
                         avgOfWinnings(statement);
-
                         break;
                     }
                     case 2: {
-                        // Probably overkill, but this way we can check for a correct date:
                         LocalDate date = getDateInput();
-
-                        String strdate = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
-                        System.out.println(strdate);
-
+                        String specificDate = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+                        System.out.println(specificDate); // for debugging
                         String statement = "SELECT stake " +
                                 "FROM wagers " +
                                 "WHERE " +
-                                    "selection = (SELECT selection " +
-                                    "FROM wagers AS w1, events AS e " +
-                                    "WHERE e.event_id = w1.event_id AND w1.selection <> e.outcome) " +
-                                "AND date_of_wager = \'" + strdate + "\';";
-
+                                "selection = (SELECT selection " +
+                                "FROM wagers AS w1, events AS e " +
+                                "WHERE e.event_id = w1.event_id AND w1.selection <> e.outcome) " +
+                                "AND date_of_wager = \'" + specificDate + "\';";
                         avgOfWinnings(statement);
-
                         break;
                     }
                     case 3: {
                         String statement = "SELECT stake " +
                                 "FROM wagers " +
                                 "WHERE " +
-                                    "selection = (SELECT selection " +
-                                    "FROM wagers AS w1, events AS e " +
-                                    "WHERE e.event_id = w1.event_id AND w1.selection <> e.outcome)" +
+                                "selection = (SELECT selection " +
+                                "FROM wagers AS w1, events AS e " +
+                                "WHERE e.event_id = w1.event_id AND w1.selection <> e.outcome)" +
                                 ";";
-
                         avgOfWinnings(statement);
-
                         break;
                     }
+                    default :
+                        System.out.println("Invalid Choice!!");
                 }
                 break;
             case 2:
                 System.out.println("Find cheaters!");
-
-                String dbLocation = "jdbc:sqlite:src/shelby_v1.db";
-
-                try (Connection conn = DriverManager.getConnection(dbLocation); Statement stmt = conn.createStatement()) {
+                try (Connection conn = DriverManager.getConnection("jdbc:sqlite:src/shelby_v1.db"); Statement stmt = conn.createStatement()) {
                     String strSelect =
                             "SELECT gambler_id " +
                                     "FROM wagers " +
-                                    "WHERE " +
-                                        "selection = (SELECT selection " +
+                                    "WHERE selection = " +
+                                       "(SELECT selection " +
                                         "FROM wagers AS w1, events AS e " +
                                         "WHERE e.event_id =  w1.event_id  AND w1.selection = e.outcome ) " +
-                                    "ORDER BY stake; ";
-
+                                    "ORDER BY gambler_id; ";
                     ResultSet x = stmt.executeQuery(strSelect);
                     HashMap<String, Integer> myMap = new HashMap<String, Integer>();
-
                     while (x.next()) {
                         int curVal;
                         if (myMap.containsKey(x.getString("gambler_id"))) {
@@ -110,117 +90,82 @@ public class App {
                         } else
                             myMap.put(x.getString("gambler_id"), 1);
                     }
-
                     for (String k : myMap.keySet())
-                        if (myMap.get(k) >= 3){
-//                            System.out.println("CHEATER ID: " + k);
-
-                            // todo this doesn't print?
-                            strSelect =
-                                    "SELECT name, address FROM gamblers WHERE gambler_id = \'"+k+"\';";
-                            ResultSet addresses = stmt.executeQuery(strSelect);
-                            while (addresses.next()) {
-                                System.out.println(String.format("CHEATER ID: %s, NAME: %s, %s", k, addresses.getString("name"), addresses.getString("address")));
-//                                System.out.println("CHEATER ADDRESS "+ addresses.getString("address"));
-                            }
+                        if (myMap.get(k) >= 3) {
+                            //System.out.println("CHEATER ID: " + k);
+                            // todo this doesn't print? ==> It is because of the use of name Or it could be the Ruleset/ Statement concurrency problems causing this in the background. We should discuss this!
+                            strSelect = "SELECT name, address FROM gamblers WHERE gambler_id = \'" + k + "\';";
+                            x = stmt.executeQuery(strSelect);
+                            while (x.next())
+                                System.out.println(String.format("CHEATER ID: %s, NAME: %s, %s", k, x.getString("name"), x.getString("address")));
                         }
                 } catch (SQLException ex) { ex.printStackTrace(); }
-
                 break;
             case 3:
-                System.out.println("Popularity of matches"); break;
+                System.out.println("Popularity of matches"); //TODO: TO IMPLEMENT TODAY!
+                break;
+            default :
+                System.out.println("Invalid Choice!!");
         }
     }
 
     private static ArrayList<String> doSQLStatement(String statement) {
-        String dbLocation = "jdbc:sqlite:src/shelby_v1.db";
-
-        try (Connection connection = DriverManager.getConnection(dbLocation);
-             Statement stmt = connection.createStatement()) {
-
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:src/shelby_v1.db"); Statement stmt = connection.createStatement()) {
             ResultSet resultSet = stmt.executeQuery(statement);
             ArrayList<String> results = new ArrayList<>();
-
-            while (resultSet.next()) {
+            while (resultSet.next())
                 results.add(resultSet.getString("event_id"));
-            }
-
             return results;
         } catch (SQLException e) {
             e.printStackTrace();
-
-            return null;
         }
+        return null;
     }
 
     private static int getMenuChoice(String questions) {
         System.out.println(questions);
-
         while (true) {
             Scanner reader = new Scanner(System.in);
-
             System.out.print("Make your choice: ");
             String line = reader.nextLine();
-
-            try {
-                return Integer.valueOf(line);
-            } catch (NumberFormatException n) {
-                System.out.println("\nNot a number, please enter the option number\n");
-            }
+            try { return Integer.valueOf(line);
+            } catch (NumberFormatException n) { System.out.println("\nNot a choice, please enter the option number! TRY AGAIN!\n"); }
         }
     }
 
     private static String getEventIDInput(String questions, ArrayList<String> options) {
         System.out.println(questions);
-
         while (true) {
             Scanner reader = new Scanner(System.in);
-
             System.out.print("Make your choice: ");
             String line = reader.nextLine();
-
-            if (options.contains(line)) {
+            if (options.contains(line))
                 return line;
-            } else
-                System.out.println("That event ID does not exist");
+            else
+                System.out.println("That event ID does not exist! TRY AGAIN!");
         }
     }
 
     private static LocalDate getDateInput() {
-        while (true) {
-            Scanner reader = new Scanner(System.in);
-
-            System.out.print("\nEnter the date YYYY-MM-DD: ");
-            String line = reader.nextLine();
-
-            try {
-                LocalDate date = LocalDate.parse(line);
-                return date;
-            } catch (DateTimeParseException dtpe) {
-                System.out.println("Date not entered correctly, please try again and use YYYY-MM-DD");
-            }
-        }
+        Scanner reader = new Scanner(System.in);
+        System.out.print("\nEnter the date YYYY-MM-DD: ");
+        String line = reader.nextLine();
+        try { return LocalDate.parse(line);
+        } catch (DateTimeParseException dtpe) { System.err.println("Date not entered correctly, please try again and use YYYY-MM-DD"); }
+        return null;
     }
 
     private static void avgOfWinnings(String statement) {
-        String dbLocation = "jdbc:sqlite:src/shelby_v1.db";
-
-        try (Connection connection = DriverManager.getConnection(dbLocation);
-             Statement stmt = connection.createStatement()) {
-
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:src/shelby_v1.db"); Statement stmt = connection.createStatement()) {
             ResultSet resultSet = stmt.executeQuery(statement);
-            int sum = 0;
-            int count = 0;
+            int sum = 0, count = 0;
             while (resultSet.next()) {
-                int stake = resultSet.getInt("stake");
-                sum += stake;
-                System.out.println(stake);
+                sum += resultSet.getInt("stake");
+                System.out.println(resultSet.getInt("stake")); // for debugging
                 count++;
             }
-
-            System.out.println("Average: " + sum / count);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+            System.out.println("Total Winnings: $$ " + sum );
+            System.out.println("Average House Winnings: ~~" + (sum / count));
+        } catch (SQLException e) { e.printStackTrace(); }
     }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -32,7 +32,6 @@ public class Program {
                 "confidence INTEGER NOT NULL," +
                 "PRIMARY KEY (contender_id)" +
                 ");";
-
         String eventTable = "CREATE TABLE IF NOT EXISTS events(" +
                 "event_id VARCHAR(255) NOT NULL," +
                 "outcome INTEGER NOT NULL," +
@@ -44,15 +43,12 @@ public class Program {
                 "FOREIGN KEY (contender_1_id) REFERENCES contenders(contender_id)," +
                 "FOREIGN KEY (contender_2_id) REFERENCES contenders(contender_id)" +
                 ");";
-
         String gamblerTable = "CREATE TABLE IF NOT EXISTS gamblers(" +
                 "gambler_id VARCHAR(255) NOT NULL," +
                 "name VARCHAR(255) NOT NULL," +
                 "address VARCHAR(255) NOT NULL," +
                 "PRIMARY KEY(gambler_id)" +
                 ");";
-
-        // TODO timestamps are weird, because thomas shelby lives in 1919 where no computers exists
         String wagerTable = "CREATE TABLE IF NOT EXISTS wagers(" +
                 "wager_id VARCHAR(255) NOT NULL," +
                 "event_id VARCHAR(255) NOT NULL," +
@@ -65,20 +61,16 @@ public class Program {
                 "FOREIGN KEY (event_id) REFERENCES events(event_id)," +
                 "FOREIGN KEY (gambler_id) REFERENCES gamblers(gambler_id)" +
                 ");";
-
         try (Connection conn = DriverManager.getConnection(dbLocation); Statement stmt = conn.createStatement()) {
             stmt.execute(gamblerTable);
             stmt.execute(wagerTable);
             stmt.execute(eventTable);
             stmt.execute(contenderTable);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        } catch (SQLException e) { System.out.println(e.getMessage()); }
     }
 
     private static int EventWins(String dbLocation, String eventId) {
         try (Connection conn = DriverManager.getConnection(dbLocation); Statement stmt = conn.createStatement();) {
-
             String strSelect;
             if (eventId != null)
                 strSelect =
@@ -89,7 +81,6 @@ public class Program {
                                 "FROM wagers AS w1, events " +
                                 "WHERE events.event_id =  \'" + eventId + "\'   AND w1.selection <> events.outcome )" +
                                 "ORDER BY stake; ";
-
             else strSelect =
                     "SELECT wager_id, odds, gambler_id, stake " +
                             "FROM wagers " +
@@ -98,14 +89,11 @@ public class Program {
                             "FROM wagers AS w1, events AS e " +
                             "WHERE e.event_id =  w1.event_id  AND w1.selection <> e.outcome )" +
                             "ORDER BY stake; ";
-            System.out.println("\nThe SQL query is: " + strSelect + "\n"); // Echo For debugging
-
+            System.out.println("\nThe SQL query is: " + strSelect + "\n");
             ResultSet rset = stmt.executeQuery(strSelect);
-            //Process the ResultSet by scrolling the cursor forward via next().
-            //For each row, retrieve the contents of the cells with getXxx(columnName).
             System.out.println("Wagers Dropped:");
             int MulaMade = 0;
-            while (rset.next()) {   // Move the cursor to the next row, return false if no more row
+            while (rset.next()) {
                 System.out.print("Booked Wager Nr°: " + rset.getString("wager_id"));
                 System.out.print(", Player id: " + rset.getString("gambler_id"));
                 MulaMade += rset.getInt("stake");
@@ -114,9 +102,7 @@ public class Program {
             }
             System.out.println("\nTotal House winnings from events = " + MulaMade);
             return MulaMade;
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        } catch (SQLException ex) { ex.printStackTrace(); }
         return 0;
     }
 
@@ -133,7 +119,6 @@ public class Program {
                                 "FROM wagers , events " +
                                 "WHERE events.event_id =  \'" + eventId + "\'   AND wagers.selection = events.outcome )" +
                                 "ORDER BY stake; ";
-
             else strSelect =
                     "SELECT wager_id, odds, gambler_id, stake " +
                             "FROM wagers " +
@@ -142,23 +127,20 @@ public class Program {
                             "FROM wagers AS w1, events AS e " +
                             "WHERE e.event_id =  w1.event_id  AND w1.selection = e.outcome )" +
                             "ORDER BY stake; ";
-            System.out.println("\nThe SQL query is: " + strSelect + "\n"); // Echo For debugging
+            System.out.println("\nThe SQL query is: " + strSelect + "\n");
             ResultSet rset = stmt.executeQuery(strSelect);
             System.out.println("Wagers to be fulfilled:");
             int loss = 0;
-            while (rset.next()) {   // Move the cursor to the next row, return false if no more row
+            while (rset.next()) {
                 System.out.print("Booked Wager Nr°: " + rset.getString("wager_id"));
                 System.out.print(", Player id: " + rset.getString("gambler_id"));
                 loss += rset.getInt("odds") * rset.getInt("stake");
                 System.out.println(", odds when bet was made by player: " + rset.getInt("odds"));
                 System.out.println(", AMount placed by player * odds " + rset.getInt("stake"));
-
             }
             System.out.println("\nTotal House losses from events = " + loss);
             return loss;
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        } catch (SQLException ex) { ex.printStackTrace(); }
         return 0;
     }
 
@@ -182,6 +164,7 @@ public class Program {
 
         String line;
         JSONObject jsonObject;
+
         /*Insert into the events Table*/
         InputStream inputStream = new FileInputStream(new File("src/events.json"));
         Objects.requireNonNull(inputStream, "InputStream cannot be null");
@@ -201,6 +184,7 @@ public class Program {
         }
         ppstmtEvent.executeBatch();
         conn.commit();
+
         /*Insert into the gamblers Table*/
         inputStream = new FileInputStream(new File("src/gamblers.json"));
         Objects.requireNonNull(inputStream, "InputStream cannot be null");
@@ -217,6 +201,7 @@ public class Program {
         }
         ppstmtGambler.executeBatch();
         conn.commit();
+
         /*Insert into the wagers Table*/
         inputStream = new FileInputStream(new File("src/wagers.json"));
         Objects.requireNonNull(inputStream, "InputStream cannot be null");
@@ -273,8 +258,7 @@ public class Program {
 
     private static void collect(String dbLocation, String k) {
         try (Connection xx = DriverManager.getConnection(dbLocation); Statement stmt = xx.createStatement();) {
-            String strSelect =
-                    "SELECT address FROM gamblers AS x WHERE x.gambler_id = \'"+k+"\';";
+            String strSelect = "SELECT address FROM gamblers AS x WHERE x.gambler_id = \'"+k+"\';";
             ResultSet x = stmt.executeQuery(strSelect);
             while (x.next()) {
                 System.out.println("CHEATER NAME "+x.getString("name"));
@@ -282,6 +266,7 @@ public class Program {
             }
         } catch (SQLException ex) { ex.printStackTrace(); }
     }
+
     public static void main(String[] args) {
         //try { DataCreator.createWagers(); } catch (IOException e) { e.printStackTrace(); }
         String tableName = "shelby_v1.db";
